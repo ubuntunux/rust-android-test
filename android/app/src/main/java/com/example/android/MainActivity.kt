54ce6c4e2eae1623ae888606b4748c6926773233
ubuntunux <ubuntunux@gmail.com>
2021-01-23 13:39:51 +0900

    first update

---
+++ android/app/src/main/java/com/example/android/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        System.loadLibrary("rust")
+        Log.d("rust", hello("World"))
+    }
+
+    external fun hello(to: String): String
+}
